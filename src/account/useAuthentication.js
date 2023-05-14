ea32c1969279879dda019b55554ccd1c9c6a0125
MarkQuark <marcosortega0490@gmail.com>
2023-05-13 23:23:27 -0400

    Changed firebaseConfig
    Changed environment variables to be able to embed into Cloudflare Pages.

--- src/account/useAuthentication.js
+++ src/account/useAuthentication.js
@@ -7,15 +7,14 @@ import { getAuth, onAuthStateChanged} from "firebase/auth";
 
 // Your app's Firebase configuration (HARDCODED. NEEDS REVISION)
 // Left below as a placeholder.
-  const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.FIREBASE_DATABASE_URL,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
-  };
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+};
 
 // Initialize Firebase App instance; connect to Firebase services.
 const app = initializeApp(firebaseConfig);
